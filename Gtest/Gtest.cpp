b95e69ef92d402c55ebfd32d64881fc90834abc3
liutao <751226701@qq.com>
2024-03-01 09:53:22 +0800

    优化

--- Gtest/Gtest.cpp
+++ Gtest/Gtest.cpp
@@ -66,6 +66,56 @@ public:
 std::ofstream logFile("C:\\Users\\gd09186\\Desktop\\test.log", std::ios::app);  // 打开文件时使用追加模式
 CoutTeeBuffer teeBuffer(std::cout.rdbuf(), logFile.rdbuf());
 std::ostream tee(&teeBuffer);
+//测试指定点的温度
+void testPointTemp(SGP_HANDLE handle)
+{
+    float standTemp = 20;
+    float k = 30;
+    int counts = 0;
+
+    while (1) {
+        const int pointNum = 1;
+        int* index = (int*)malloc(pointNum * sizeof(int));
+        index[0] = 163840;
+
+        int ret = SGP_SetTempPoints(handle, index, pointNum, 1);
+        if (ret == SGP_OK)
+        {
+            float* temp = (float*)malloc(pointNum * sizeof(float));
+            memset(temp, 0x00, pointNum * sizeof(float));
+            if (temp != NULL)
+            {
+                int ret = SGP_GetTempPoints(handle, temp, pointNum, 1);
+                if (ret == SGP_OK)
+                {
+                    for (int i = 0; i < pointNum; i++)
+                    {
+                        float dif = std::abs(temp[i] - standTemp);
+                        if (dif > k)
+                        {
+                            counts++;
+                        }
+                        tee << fixed << setprecision(1) << "实时温度：" << temp[i] << "   比较标准：" << standTemp
+                            << "   差值：" << dif << "   误差范围：" << k << "   异常温度次数" << counts << endl;
+                    }
+                }
+                else
+                {
+                    cout << "获取温度失败！ret is:" << ret << endl;
+                }
+            }
+            free(temp);
+            temp = NULL;
+        }
+        else
+        {
+            cout << "设置索引失败！ret is:" << ret << endl;
+        }
+        free(index);
+        index = NULL;
+        Sleep(1000);
+    }
+}
 
 int main()
 {
@@ -78,7 +128,7 @@ int main()
     int n;
     cout << "Please enter the interval time(ms): " << endl;
     cin>>n;*/
-    const char* server = "192.168.1.151";
+    const char* server = "192.168.21.4";
     const char* username = "root";
     const char* password = "guide123";
     int port = 80;
@@ -139,54 +189,11 @@ int main()
         cout << "测温版本：" << info.measure_version << endl;
         cout << "SDK版本： " << info.sdk_version << endl;
 
-        float standTemp = 230;
-        float k = 30;
-        int counts = 0;
         
-        while (1) {
-            const int pointNum = 1;
-            int* index = (int*)malloc(pointNum * sizeof(int));
-            index[0] = 163840;
 
-            int ret = SGP_SetTempPoints(handle, index, pointNum, 1);
-            if (ret == SGP_OK)
-            {
-                float* temp = (float*)malloc(pointNum * sizeof(float));
-                memset(temp, 0x00, pointNum * sizeof(float));
-                if (temp != NULL)
-                {
-                    int ret = SGP_GetTempPoints(handle, temp, pointNum, 1);
-                    if (ret == SGP_OK)
-                    {
-                        for (int i = 0; i < pointNum; i++)
-                        {
-                            float dif = std::abs(temp[i] - standTemp);
-                            if (dif > k) 
-                            {
-                                counts++;
-                            }
-                            tee << fixed << setprecision(1) << "实时温度：" << temp[i] << "   比较标准：" << standTemp
-                                << "   差值：" << dif << "   误差范围："<< k << "   异常温度次数" << counts << endl;
-                        }
-                    }
-                    else
-                    {
-                        cout << "获取温度失败！ret is:" << ret << endl;
-                    }
-                }
-                free(temp);
-                temp = NULL;
-            }
-            else
-            {
-                cout << "设置索引失败！ret is:" << ret << endl;
-            }
-            free(index);
-            index = NULL;
-            Sleep(1000);
-        }
+        
 
-
+       
 
 
 
